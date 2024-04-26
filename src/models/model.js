5f740abcf719f337124e1b5480106dd2c989e728
Dimas Vindyawan <dimasvindyawan@gmail.com>
2024-04-26 17:58:11 +0700

    Add alias

--- src/models/model.js
+++ src/models/model.js
@@ -1,7 +1,7 @@
 const { db } = require("../config/db")
 
 const queryAll = async () => {
-    const QUERY = `SELECT nama_peserta, nama_perusahaan 
+    const QUERY = `SELECT nama_peserta AS nama, nama_perusahaan AS perusahaan
                    FROM peserta 
                    INNER JOIN perusahaan 
                    ON peserta.id_perusahaan=perusahaan.id`
